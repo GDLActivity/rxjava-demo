5cbaeff9f639aedec96cafd0dc1764e88db9665f
enchf <e.ernesto.espinosa@gmail.com>
2017-02-08 18:57:56 -0600

    Service completed

--- src/gdlactivity/rx/Main.java
+++ src/gdlactivity/rx/Main.java
@@ -1,10 +1,18 @@
 package gdlactivity.rx;
 
 import gdlactivity.service.GithubService;
+import gdlactivity.service.Profile;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -16,10 +24,42 @@ public class Main {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(GithubService.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
 
+        int random = new Random().nextInt();
         GithubService service = retrofit.create(GithubService.class);
+        Observable<List<Profile>> observable = service.getUsers(random);
 
-        System.out.println(service.getUsers(new Random().nextInt()).execute().body());
+        observable
+                .flatMap(new Func1<List<Profile>, Observable<Profile>>() {
+                    @Override
+                    public Observable<Profile> call(List<Profile> profiles) {
+                        return Observable.from(profiles);
+                    }
+                }).flatMap(new Func1<Profile, Observable<List<Profile>>>() {
+                    @Override
+                    public Observable<List<Profile>> call(Profile profile) {
+                        return service.getFollowers(profile.getLogin());
+                    }
+                })
+                .filter(l -> l.size() > 1)
+                .subscribe(new Observer<List<Profile>>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("Completed");
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        System.out.println(t.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(List<Profile> profiles) {
+                        profiles.stream().forEach(System.out::println);
+                    }
+                })
+        ;
     }
 }
--- src/gdlactivity/service/GithubService.java
+++ src/gdlactivity/service/GithubService.java
@@ -4,6 +4,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import rx.Observable;
 
 import java.util.List;
 
@@ -15,5 +16,8 @@ public interface GithubService {
     public static final String BASE_URL = "https://api.github.com/";
 
     @GET("users")
-    Call<List<Profile>> getUsers(@Query("since") int since);
+    Observable<List<Profile>> getUsers(@Query("since") int since);
+
+    @GET("users/{user}/followers")
+    Observable<List<Profile>> getFollowers(@Path("user") String user);
 }
