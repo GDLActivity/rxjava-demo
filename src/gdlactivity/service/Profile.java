f4d03f833063383d32cf22338b6a92a0ec7e389c
enchf <e.ernesto.espinosa@gmail.com>
2017-02-08 18:33:45 -0600

    Added followers URL

--- src/gdlactivity/service/Profile.java
+++ src/gdlactivity/service/Profile.java
@@ -1,5 +1,7 @@
 package gdlactivity.service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Github Profile.
  */
@@ -8,10 +10,15 @@ public class Profile {
     private int id;
     private String login;
     private String url;
+    @SerializedName("followers_url")
+    private String followersUrl;
 
-    @Override
-    public String toString() {
-        return String.format("%s [%s]", login, url);
+    public String getFollowers() {
+        return followersUrl;
+    }
+
+    public void setFollowers(String followersUrl) {
+        this.followersUrl = followersUrl;
     }
 
     public int getId() {
@@ -36,5 +43,10 @@ public class Profile {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s [%s], followers: %s", login, url, followersUrl);
     }
 }
